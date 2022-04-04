b35959a1db0b0aec16c7e81c8631b51e3d0bdcf6
dusk <52311860+MC-dusk@users.noreply.github.com>
2022-04-04 15:31:41 +0800

    Add files via upload

---
+++ calendarPrinter.py
@@ -0,0 +1,61 @@
+# 日历打印器 v3.0
+# calendar printer v3.0
+
+# 本地时间
+
+import time
+
+def getTime():
+    localtime = time.strftime("%Z %Y-%m-%d %A %H:%M:%S", time.localtime())
+    return localtime
+
+# 文件输出
+
+import calendar
+
+def saveFile():
+    try:
+        if type(eval(et.get())) == int:
+            year = int(et.get())
+            file = open(str(year) + ".txt", "w")
+            file.write(calendar.calendar(year,w=3,l=1,c=6) + '\n')
+            printTime = getTime()
+            file.write(u'打印时间：' + printTime)
+            file.close()
+            mb.showinfo('提示','输出成功！',icon = mb.INFO)
+    except:
+        et.delete(0,'end')
+        mb.showerror('提示','请正确输入！',icon = mb.ERROR)
+
+# GUI界面
+
+import tkinter as tk
+from tkinter import messagebox as mb
+
+def gui(root):
+    fr = tk.Frame(root, relief='groove')
+    fr.place(relx=0.5, rely=0.5, anchor='center')
+
+    lb = tk.Label(fr, text = "日历打印器\n\n请输入要打印的年份：", bd=20, font=(20), fg="red", bg="yellow")
+    lb.grid(row=0,column=0,columnspan=2)
+
+    global et
+    et = tk.Entry(fr)
+    et.insert(0,time.strftime('%Y'))
+    et.grid(row=1,column=0,columnspan=2)
+
+    bt = tk.Button(fr, text = "保存到当前目录", command = saveFile)
+    bt.grid(row=3,column=0)
+
+    quit = tk.Button(fr, text = "退出", command = root.quit)
+    quit.grid(row=3,column=1)
+
+def main():
+    root = tk.Tk()
+    root.title("日历打印器 v3.0")
+    # root.iconbitmap(".\\\calendar.ico")
+    root.geometry('320x180+200+200')
+    gui(root)
+    root.mainloop()
+
+main()
---
+++ readme.md
@@ -0,0 +1,34 @@
+## 日历打印器 v3.1
+
+> 对着《程序设计思想与方法》一步步抄出来的，边写边学，原来是python2的代码，对应python3进行了改写。
+
+- 功能：输入年份（**v3.0之前** 1900之后），打印日历（**v1.0** 命令行 / **v2.0及之后** txt文本文件）。
+
+#### 编译：
+
+- 用`pyinstaller -F calendar_v1.0.py`方式打包生成exe（7.52M）。
+- 用`pyinstaller -F calendar_v2.0.py`方式打包生成exe（10.1M）。
+- 用`pyinstaller -F -w -i calendar.ico calendar_v3.0.py`方式打包生成`calendar_v3.0.1.exe`（10.1M）。
+- 用`pyinstaller -F -w -i calendar.ico calendar_v3.0.py --upx-dir <DIR>`方式打包生成`calendar_v3.0.2.exe`（8.97M）。
+- 用`pyinstaller -F -w -i calendar.ico calendar_v3.1.py`方式打包生成`calendar_v3.1.1.exe`（10.1M）。
+- 用`upx calendar_v3.1.1.exe`方式压缩生成`calendar_v3.1.2.exe`（10.0M）。
+- 用`pyinstaller -F -w -i calendar.ico calendar_v3.1.py --upx-dir <DIR>`方式打包生成`calendar_v3.1.3.exe`（8.97M）。
+- exe文件：https://wwx.lanzoui.com/b01ihdbmd (52pj)
+
+#### 截图：
+
+<img src="https://z3.ax1x.com/2021/07/16/WQZdsK.png" alt="v1.0" style="zoom:50%;" />
+
+<img src="https://z3.ax1x.com/2021/07/16/WQZNxx.jpg" alt="v2.0" style="zoom:50%;" />
+
+[![v3.0](https://z3.ax1x.com/2021/07/18/W3ZWHf.png)](https://imgtu.com/i/W3ZWHf)
+
+[![v3.1](https://z3.ax1x.com/2021/07/19/WJo70x.png)](https://imgtu.com/i/WJo70x)
+
+#### 其他：
+
+周数可能还能实现，农历有点困难，毕竟calendar模块是没有农历的，我自己也不懂农历。
+
+> [xxdahai](https://www.52pojie.cn/home.php?mod=space&uid=107128) 农历用这个模块：zhdate  发表于 2021-8-13 11:27
+
+#### END
